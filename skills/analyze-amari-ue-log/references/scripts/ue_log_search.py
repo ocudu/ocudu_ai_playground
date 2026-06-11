e076b059d35b6a2df9128df3730431a7f26abf8d
Francisco Paisana <francisco.paisana@srs.io>
2026-06-11 21:54:54 +0200

    add copyright header
    Signed-off-by: Francisco Paisana <francisco.paisana@srs.io>

--- skills/analyze-amari-ue-log/references/scripts/ue_log_search.py
+++ skills/analyze-amari-ue-log/references/scripts/ue_log_search.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+# SPDX-License-Identifier: BSD-3-Clause-Open-MPI
+
 """
 ue_log_search.py - Search Amarisoft UE log with filtering.
 
